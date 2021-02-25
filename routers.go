7ac8d23b47068f64e448c2b494ab9897bb817831
氘化氢 <36532302+HydrogenDeuterium@users.noreply.github.com>
2021-02-25 20:28:33 +0800

    之前配置错了导致只能单文件运行，现在弄好了

--- routers.go
+++ routers.go
@@ -7,5 +7,6 @@ import (
 
 func CollectRoute(router *gin.Engine) *gin.Engine {
 	router.POST("/api/auth/register", controller.Register)
+	router.POST("/api/auth/login", controller.Login)
 	return router
 }
