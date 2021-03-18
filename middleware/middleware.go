6b31b3009f1458db5e8c17b8c0694788a79092c8
氘化氢 <36532302+HydrogenDeuterium@users.noreply.github.com>
2021-03-18 16:08:14 +0800

    增加对bearer token的说明

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,22 +3,30 @@ package middleware
 import (
 	"DemoProjectGO/common"
 	"DemoProjectGO/util"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 //AuthMiddleware did sth i know less.
 func AuthMiddleware() gin.HandlerFunc {
 	return func(context *gin.Context) {
-
-		sToken := context.GetHeader("Authorization")
-
 		Abort401 := func() {
 			context.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
 			// 中断请求
 			context.Abort()
 		}
+
+		sToken := context.GetHeader("Authorization")
+
+		//rfc6750d的规范要求bearer token以“bearer ”开头
+		//参见：https://tools.ietf.org/html/rfc6750
+		//这里我们暂时不使用这个特性
+		//sPrefixToken := context.GetHeader("Authorization")
+		//if !strings.HasPrefix(sPrefixToken, "bearer") {
+		//	Abort401()
+		//}
+		//sToken :=sPrefixToken[7:0]
+
 		if sToken == "" {
 			Abort401()
 			return
